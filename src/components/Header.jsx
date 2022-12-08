e469ba374bc32733d7a42c9f1e6cffcade623c72
Klaercio <klaerciosilva@gmail.com>
2022-12-08 19:41:06 -0300

    ✨ feat: Adicionado o cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import Logo from '../assets/ignite-logo.svg';
 
 export default function Header() {
     return <>
         <header className={styles.header}>
+          <img src={Logo} alt="Logo da página"/>
           <strong >Ignite Feed</strong>
         </header>
     </>;
